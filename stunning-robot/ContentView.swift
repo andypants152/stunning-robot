3eedce05a5a220e25d9b00894563728f51d002eb
AndyPants <andypants152@gmail.com>
2026-07-04 00:42:33 -0400

    Initial Commit

---
+++ stunning-robot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stunning-robot
+//
+//  Created by Andy Meyer on 7/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stunning-robot/stunning_robotApp.swift
@@ -0,0 +1,17 @@
+//
+//  stunning_robotApp.swift
+//  stunning-robot
+//
+//  Created by Andy Meyer on 7/4/26.
+//
+
+import SwiftUI
+
+@main
+struct stunning_robotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
